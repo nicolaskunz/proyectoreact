33d4acde1f9d55960262da146d8d9b8ef586f965
nicolaskunz <kunz.-@hotmail.com>
2020-09-13 12:11:39 -0300

    sumamos Useeffect y Settimeout

--- src/components/ItemList/ItemList.jsx
+++ src/components/ItemList/ItemList.jsx
@@ -3,22 +3,26 @@ import Item from './Item.jsx';
 
 
 export function ItemList() {
+    const [productos, setProductos] = useState([]);  const fnWithPromise = () => {
+      return new Promise((resolve, reject) => {
+        return setTimeout(() => {
+          const fakeList = [
+            { id: "1", name: "producto1" },
+            { id: "7", name: "producto7" },
+            { id: "4", name: "producto4" },
+          ];        return resolve(fakeList);
+        }, 2000);
+      });
+    };  useEffect(() => {
+      fnWithPromise().then((productos) => setProductos(productos));
+    }, []);  const listaProductos = productos.map((productos) => (
+      <Item id={productos.id} name={productos.name} />
+    ));  return <ul>{listaProductos}</ul>;
+  }
 
-    const productos =  [
-        {id: '1', name: 'producto1'},
-        {id: '7', name: 'producto7'},
-        {id: '4', name: 'producto4'},
-    ];
-     
-    const listaProductos = productos.map((productos) =>
-        <Item id={productos.id} name={productos.name} />
-    );
 
-    return <ul>{listaProductos}</ul>
-    
-
-}
-
+  
+// Ejemplo basico
 // Nunca me figura este console log:
 
 // setTimeout(() => {
@@ -26,6 +30,7 @@ export function ItemList() {
 // }, 2000);
 
 
+// ejemplo que dio el profe:
 // const task = new Promise((resolve, reject) => {
 
     // setTimeout(() => {
